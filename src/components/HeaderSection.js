73764c3bb974461ca0158ca385e0ecb6e9d3b325
Ben <bcflock@gmail.com>
2021-11-24 22:29:58 -0500

    prewebsite

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -10,7 +10,8 @@ import Confirmation from './Confirmation';
 import { Redirect } from 'react-router-dom';
 import { rgbToHex } from '@material-ui/core';
 import Countdown from './Countdown';
-
+import { Link } from 'react-router-dom';
+import Register from './Register';
 class HeaderSection extends Component {
 
 
@@ -63,8 +64,10 @@ class HeaderSection extends Component {
                       //<div id = "CWRU" className = "bigClearText">Case Western Reserve University</div> 
                       }
                         <div id = "date" className = "bigClearText">February 4-6th 2022</div>
-                        <div className = "bigClearText">Check-in: 5:30PM </div>
                         <div className = "bigClearText"><a href="https://www.google.com/maps/place/Sears+think%5Bbox%5D/@41.5005757,-81.6078735,17z/data=!3m1!4b1!4m5!3m4!1s0x8830fb871a38380b:0xed457fd242d00e1a!8m2!3d41.5005757!4d-81.6056848">Sears think[box]</a></div>
+                        <div className = "bigClearText">More info coming soon...</div>
+                        <div className = "bigClearText">Sign up for our mailing list</div>
+                        <div className = "bigClearText">to receive updates!</div>
                       </div>
                     </div>
                     {
@@ -91,7 +94,7 @@ class HeaderSection extends Component {
               <div className = "registerButtonGroup">
               
               {
-                <button onClick={
+                /*<button onClick={
                   //this._onButtonClick.bind(this)
                   this._onButtonClick.bind( (e) => {
                     e.preventDefault();
@@ -99,7 +102,10 @@ class HeaderSection extends Component {
                   } )
                 } 
                 id="registerBtn">Register
-                </button>
+                </button>*/
+                <a id="registerBtn" href="https://docs.google.com/forms/d/e/1FAIpQLSeRPp1tLWAgMvsGbwPTGD2nfasxWoi0YXOfX7zn9T5viDp3Gg/viewform?usp=sf_link">
+                  SIGN ME UP
+                </a>
               //<button id="registerBtn">Register</button>
               }
               {/*<CoCPopUp showCodeOfConductBox={this.state.showCodeOfConductBox}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from './components/Register';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+    <div>
+        <Route path='/' component={App}/>
+        <Route path='/register' component={Register}/>
+    </div>
+</Router>
+, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
